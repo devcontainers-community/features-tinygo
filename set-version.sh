d071233c146462b6e616ed66ea4e49648d4e1936
Jacob Hummer <jcbhmr@outlook.com>
2023-08-16 20:42:00 -0500

    pull out tinygo

--- set-version.sh
+++
@@ -1,3 +0,0 @@
-echo "Setting Version to $1"
-
-grep -RiIl '\"version\": ' | xargs sed -i "s/[0-9]\+[.][0-9]\+[.][0-9]\+/$1/g"
--- src/spin-message-trigger/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-spin plugin install --url https://github.com/lee-orr/spin-message-trigger/releases/download/canary/trigger-message.json --yes
