9da047b20f828c0542bf810973fd3f19e794fddf
Jacob Hummer <jcbhmr@outlook.com>
2023-08-31 14:59:24 -0500

    replace latest with actual "1.2.3" from github releases latest tagged version

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 set -ex
+source lib.sh
 
-echo "Activating feature 'Tiny Go'"
+check_packages curl ca-certificates
 
-# Tiny Go version
-VERSION=${VERSION:-"0.28.1"}
-ARCHITECTURE="amd64"
+arch=$(dpkg --print-architecture)
 
-if [ "$(dpkg --print-architecture)" ==  "arm64" ]; then
-    ARCHITECTURE="arm64"
+if [[ ${VERSION:-latest} == latest ]]; then
+  # .../releases/latest redirects to /releases/tag/v1.2.3
+  # https://stackoverflow.com/questions/58379142/grep-how-to-output-only-the-content-of-a-capturing-group
+  VERSION=$(curl \
+    -fsSL https://github.com/tinygo-org/tinygo/releases/latest \
+    -w '%{url_effective}' -o /dev/null \
+    | sed -nr 's/\/v(.*)$/\1/p')
 fi
 
-apt update
-apt install -y curl
+url="https://github.com/tinygo-org/tinygo/releases/download/v$VERSION/tinygo_${VERSION}_$arch.deb"
 
-LOCALFILE="/tmp/tinygo_${VERSION}_${ARCHITECTURE}.deb"
-TINYURL="https://github.com/tinygo-org/tinygo/releases/download/v${VERSION}/tinygo_${VERSION}_${ARCHITECTURE}.deb"
+curl -fsSL "$url" -o tinygo.deb
+dpkg -i tinygo.deb
+rm tinygo.deb
 
-curl -Lo ${LOCALFILE} ${TINYURL}
-dpkg -i  ${LOCALFILE}
-rm  ${LOCALFILE}
+# Clean up
+rm -rf /var/lib/apt/lists/*
